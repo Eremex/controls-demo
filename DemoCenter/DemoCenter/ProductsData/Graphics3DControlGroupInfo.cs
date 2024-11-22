9d16833dafa07378a902daf878cb5cda47b58cbf
MikeXAML <eskin.mike@gmail.com>
2024-11-22 18:26:34 +0300

    New demo descriptions

--- DemoCenter/DemoCenter/ProductsData/Graphics3DControlGroupInfo.cs
+++ DemoCenter/DemoCenter/ProductsData/Graphics3DControlGroupInfo.cs
@@ -9,22 +9,22 @@ public class Graphics3DControlGroupInfo
         return new List<PageInfo>
         {
             new (name: "Overview", title: "Overview",
-                description: "TODO",
+                description: "Graphics3DControl allows you to visualize and interact with 3D models. Use the Properties pane on the right to customize the control's basic rendering and behavior settings.",
                 viewModelGetter: () => new Graphics3DControlOverviewViewModel(),
                 new VersionInfo(1, 1)),
             
             new (name: "STL Model", title: "STL Model",
-                description: "TODO",
+                description: "This example demonstrates a Graphics3DControl displaying a large 3D model loaded from an STL file. The STLDotNet6.Formats.StereoLithography library is used to parse the STL file, and to obtain data to initialize vertices and materials of the created model.",
                 viewModelGetter: () => new Graphics3DControlStlViewModel(),
                 new VersionInfo(1, 1)),
             
             new (name: "Simple Materials", title: "Simple Materials",
-            description: "TODO",
+            description: "This example demonstrates a Graphics3DControl displaying a 3D model with a simple material. Use the Properties pane on the right to customize the material settings.",
             viewModelGetter: () => new Graphics3DControlSimpleMaterialsViewModel(),
             new VersionInfo(1, 1)),
             
             new (name: "Textured Materials", title: "Textured Materials",
-                description: "TODO",
+                description: "In this example, a Graphics3DControl displays a 3D model with a textured material. Use the Materials pane on the right to choose the texture.",
                 viewModelGetter: () => new Graphics3DControlTexturedMaterialsViewModel(),
                 new VersionInfo(1, 1))
         };
--- DemoCenter/DemoCenter/ProductsData/RibbonGroupInfo.cs
+++ DemoCenter/DemoCenter/ProductsData/RibbonGroupInfo.cs
@@ -11,7 +11,7 @@ namespace DemoCenter.ProductsData
             return new List<PageInfo>()
             {
                 new PageInfo(name: "WordPadExample", title: "WordPad Example",
-                    description: "WordPad Example",
+                    description: $"RibbonControl allows you to integrate Microsoft Office-inspired navigation menus into your Avalonia UI applications. The control comes with two views: Classic (three item rows) and Simplified (one item row). The dropdown button at the control's bottom right corner opens a selector between these views. {Environment.NewLine + Environment.NewLine}RibbonControl supports multiple item types: large and small buttons, check buttons, sub-menus, in-place editors, inline and dropdown galleries, groups of buttons, and more. You can create as many pages with items as you need, and place items in the Quick Access Toolbar or the tab header area. {Environment.NewLine+ Environment.NewLine}Press the ALT key to focus the Ribbon, and then use the keyboard to navigate between Ribbon elements and activate commands.",
                     viewModelGetter: () => new WordPadExampleViewModel(),
                     new VersionInfo(1, 1), null, false)
             };
