2f9fb2daa631d68e828039426b72dfa110fdd5d2
Kalachik Roman <romankalachik@eremex.ru>
2025-02-20 13:11:22 +0300

    fix ci

---
+++ Tests/DemoCenter.Desktop.UI.Tests/AvaloniaApp.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Headless;
+using Avalonia.Threading;
+
+using DemoCenter.Desktop.UI.Tests;
+
+namespace DemoCenter.Desktop.UI.Tests
+{
+	public static class AvaloniaApp
+	{
+		public static void Stop()
+		{
+			var app = GetApp();
+			if (app is IDisposable disposable)
+			{
+				Dispatcher.UIThread.Post(disposable.Dispose);
+			}
+
+			if (app != null)
+				Dispatcher.UIThread.Post(() => app.Shutdown());
+		}
+
+		public static Window? GetMainWindow() => GetApp()?.MainWindow;
+
+		public static IClassicDesktopStyleApplicationLifetime? GetApp() =>
+			(IClassicDesktopStyleApplicationLifetime?)Application.Current?.ApplicationLifetime;
+
+		public static AppBuilder BuildAvaloniaApp() =>
+			AppBuilder
+				.Configure<App>()
+				.UsePlatformDetect()
+				.WithInterFont();
+	}
+}
---
+++ Tests/DemoCenter.Desktop.UI.Tests/emxLicense.cs
@@ -0,0 +1,16 @@
+﻿// This file is auto-generated. Please do not change it.
+using System.Runtime.CompilerServices;
+
+using Eremex.AvaloniaUI.Controls.License;
+
+namespace DemoCenter.Desktop.UI.Tests;
+public class LicenseProvider
+{
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+	[ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    public static void RegisterLicense()
+	{
+        ControlsLicenseManager.SetRuntimeLicenseOwner(new LicenseProvider(),"АО  ЭРЕМЕКС ", "F5 1F 12 8C 3B 9F 55 5C 6D F4 4A CC 2F F2 25 4E 65 54 8E 74 40 AB 7D C8 FC 1C 92 0E AE FF 45 4A 52 76 B1 B6 A0 22 96 8D");
+	}
+}
