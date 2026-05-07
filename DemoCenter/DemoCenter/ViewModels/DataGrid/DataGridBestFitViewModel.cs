f730baedbccffd9030d411c4ede390b1c1676e7e
Eremex <eremexbot@gmail.com>
2026-05-07 15:29:11 +0300

    addeed missing files

---
+++ DemoCenter/DemoCenter/ViewModels/DataGrid/DataGridBestFitViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DemoCenter.DemoData;
+
+namespace DemoCenter.ViewModels;
+
+public partial class DataGridBestFitViewModel : PageViewModelBase
+{   
+    public IList<ApparelSale> ApparelSales { get; }
+
+    public DataGridBestFitViewModel()
+    {
+        ApparelSales = DemoData.ApparelProducts.GenerateSales(5000);
+    }
+}
---
+++ DemoCenter/DemoCenter/Views/DataGrid/DataGridBestFitView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DemoCenter.Views;
+
+public partial class DataGridBestFitView : UserControl
+{
+    public DataGridBestFitView()
+    {
+        InitializeComponent();
+    }
+}
